ac2b2427e534419fb9ad21a662260194d1e318ad
rubenstryder <114153509+rubenstryder@users.noreply.github.com>
2025-12-16 00:23:35 +0100

    Added packages and tests

---
+++ test/cimsolutions.test.js
@@ -0,0 +1,35 @@
+const { nvda } = require("../node_modules/@guidepup/guidepup");
+const { chromium } = require("playwright");
+
+describe("Accessibility tests (NVDA)", () => {
+  let browser;
+  let context;
+  let page;
+  let screenReader;
+
+  beforeAll(async () => {
+    browser = await chromium.launch({ headless: false });
+    context = await browser.newContext();
+    page = await context.newPage();
+
+    screenReader = nvda;
+    await screenReader.start();
+  });
+
+  afterAll(async () => {
+    await screenReader.stop();
+    await browser.close();
+  });
+
+  test("NVDA leest de hoofdkop", async () => {
+    await page.goto("https://www.cimsolutions.nl/");
+
+    await screenReader.interact();
+    await screenReader.next();
+
+    const spoken = await screenReader.lastSpokenPhrase();
+    //expect(spoken).toMatch(/Example Domain/i);
+    //expect(spoken).toMatch(/heading/i);
+    console.log(spoken);
+  });
+});
